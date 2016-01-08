16535a60299b81286b30a400b4160b8fb215c76b
izyj <iisiramen@gmail.com>
2016-01-08 11:18:54 +0100

    factory

---
+++ PatternProj/src/controleur/factory/ShapeFactory.java
@@ -0,0 +1,34 @@
+package controleur.factory;
+
+import modele.Guerrier;
+import modele.Shape;
+import modele.Voiture;
+import modele.Keys.Keys;
+
+public class ShapeFactory {
+
+	public ShapeFactory() {
+
+
+
+
+	}
+	/**
+	 * Methode permettant de récuperer un objet de type Shape(Apparence)
+	 * @param shapeType
+	 * @return Shape
+	 */
+	public Shape getShape(String shapeType){
+
+
+		if(shapeType.equalsIgnoreCase(Keys.Shape_Guerrier)){
+	        return new Guerrier();
+
+	     } else if(shapeType.equalsIgnoreCase(Keys.Shape_Voiture)){
+	        return new Voiture();
+
+	     }
+
+	     return null;
+	}
+}
