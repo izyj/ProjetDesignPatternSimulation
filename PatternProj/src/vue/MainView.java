1f43f5af3efe30ac1fc6e5fa0ce05d90e308fe41
Gaëtan <gae.marzilli@gmail.com>
2016-01-08 10:10:30 +0100

    Ajout du menu configuration et autres

--- PatternProj/src/vue/MainView.java
+++ PatternProj/src/vue/MainView.java
@@ -2,22 +2,29 @@ package vue;
 
 import java.awt.EventQueue;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
+
 import javax.swing.JButton;
-import java.awt.GridBagLayout;
+
 import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
 import javax.swing.JLabel;
+
 import java.awt.Font;
 import java.awt.SystemColor;
+import java.io.File;
+import java.io.IOException;
+
 import javax.swing.ImageIcon;
 
 public class MainView {
@@ -65,26 +72,104 @@ public class MainView {
 		/**
 		 * Item File de la barre de menu
 		 */
-		JMenu menuFile = new JMenu("File");
-		menuBar.add(menuFile);
+		JMenu menuMenu = new JMenu("Menu");
+		menuBar.add(menuMenu);
+		
+			/**
+			 * Bouton pour nouveau jeu
+			 */
+			JMenuItem mntmNouveauJeu = new JMenuItem("Nouveau jeu");
+			mntmNouveauJeu.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					 
+				}
+			});
+			menuMenu.add(mntmNouveauJeu);
+			
+			/**
+			 * Bouton pour sauvegarder la partie
+			 */
+			JMenuItem mntmSauvgarder = new JMenuItem("Sauvegarder");
+			mntmSauvgarder.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					 
+				}
+			});
+			menuMenu.add(mntmSauvgarder);
+			
+			/**
+			 * Bouton pour fermer l'application
+			 */
+			JMenuItem mntmFermer = new JMenuItem("Fermer");
+			mntmFermer.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					 
+				}
+			});
+			menuMenu.add(mntmFermer);
 		
 		/**
-		 * Bouton pour nouveau jeu
+		 * Item de la barre de configuration
 		 */
-		JMenuItem mntmNouveauJeu = new JMenuItem("Nouveau jeu");
-		menuFile.add(mntmNouveauJeu);
+		JMenu menuConfiguration = new JMenu("Configuration XML");
+		menuBar.add(menuConfiguration);
 		
-		/**
-		 * Bouton pour sauvegarder la partie
-		 */
-		JMenuItem mntmSauvgarder = new JMenuItem("Sauvegarder");
-		menuFile.add(mntmSauvgarder);
-		
-		/**
-		 * Bouton pour fermer l'application
-		 */
-		JMenuItem mntmFermer = new JMenuItem("Fermer");
-		menuFile.add(mntmFermer);
+			/**
+			 * Bouton laby
+			 */
+			JMenuItem mntmFichierXMLLaby = new JMenuItem("Fichier XML - Labyrinthe");
+			mntmFichierXMLLaby.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					 File repertoireCourant = null;
+				        try {
+				            repertoireCourant = new File(".").getCanonicalFile();
+				            System.out.println("Répertoire courant : " + repertoireCourant);
+				        } catch(IOException e) {}
+				         
+				        JFileChooser dialogue = new JFileChooser(repertoireCourant);
+				        dialogue.showOpenDialog(null);
+				        System.out.println("Fichier choisi : " + dialogue.getSelectedFile());
+				}
+			});
+			menuConfiguration.add(mntmFichierXMLLaby);
+			
+			/**
+			 * Bouton déplacement
+			 */
+			JMenuItem mntmFichierXMLDeplacement = new JMenuItem("Fichier XML - D\u00E9placement");
+			mntmFichierXMLDeplacement.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					 File repertoireCourant = null;
+				        try {
+				            repertoireCourant = new File(".").getCanonicalFile();
+				            System.out.println("Répertoire courant : " + repertoireCourant);
+				        } catch(IOException e) {}
+				         
+				        JFileChooser dialogue = new JFileChooser(repertoireCourant);
+				        dialogue.showOpenDialog(null);
+				        System.out.println("Fichier choisi : " + dialogue.getSelectedFile());
+				}
+			});
+			menuConfiguration.add(mntmFichierXMLDeplacement);
+			
+			/**
+			 * Bouton Tower Defence
+			 */
+			JMenuItem mntmFichierXMLTowerDefence = new JMenuItem("Fichier XML - Tower Defence");
+			mntmFichierXMLTowerDefence.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					 File repertoireCourant = null;
+				        try {
+				            repertoireCourant = new File(".").getCanonicalFile();
+				            System.out.println("Répertoire courant : " + repertoireCourant);
+				        } catch(IOException e) {}
+				         
+				        JFileChooser dialogue = new JFileChooser(repertoireCourant);
+				        dialogue.showOpenDialog(null);
+				        System.out.println("Fichier choisi : " + dialogue.getSelectedFile());
+				}
+			});
+			menuConfiguration.add(mntmFichierXMLTowerDefence);
 		
 		/**
 		 * Pannel de la bannière
@@ -113,14 +198,13 @@ public class MainView {
 		btnLabyrinthe.setIcon(new ImageIcon(MainView.class.getResource("/images/laby.png")));
 		btnLabyrinthe.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+				JFileChooser dialogue = new JFileChooser();
+	            dialogue.showOpenDialog(null);
+
+	            System.out.println("Fichier choisi : " + dialogue.getSelectedFile());
 			}
 		});
-		GridBagConstraints gbc_btnLabyrinthe = new GridBagConstraints();
-		gbc_btnLabyrinthe.insets = new Insets(0, 0, 0, 5);
-		gbc_btnLabyrinthe.gridx = 5;
-		gbc_btnLabyrinthe.gridy = 7;
-		panel_menu.add(btnLabyrinthe, gbc_btnLabyrinthe);
+		panel_menu.add(btnLabyrinthe);
 		
 		/**
 		 * Simulation Deplacement
@@ -134,9 +218,6 @@ public class MainView {
 			}
 		});
 		GridBagConstraints gbc_btnDeplacement = new GridBagConstraints();
-		gbc_btnDeplacement.insets = new Insets(0, 0, 0, 5);
-		gbc_btnDeplacement.gridx = 10;
-		gbc_btnDeplacement.gridy = 7;
 		panel_menu.add(btnDeplacement, gbc_btnDeplacement);
 		
 		/**
@@ -150,10 +231,7 @@ public class MainView {
 				
 			}
 		});
-		GridBagConstraints gbc_btnTowerDefence = new GridBagConstraints();
-		gbc_btnTowerDefence.gridx = 15;
-		gbc_btnTowerDefence.gridy = 7;
-		panel_menu.add(btnTowerDefence, gbc_btnTowerDefence);
+		panel_menu.add(btnTowerDefence);
 	}
 
 }
