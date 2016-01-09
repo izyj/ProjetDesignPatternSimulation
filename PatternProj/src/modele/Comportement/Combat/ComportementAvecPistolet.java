c83b90f934b7a10abe94f3a6e5fa017d8a20ab88
Rida <user@user-PC.darty>
2016-01-09 20:52:48 +0100

    ajouter Class objet Abstrait + MAj Personnage

---
+++ PatternProj/src/modele/Comportement/Combat/ComportementAvecPistolet.java
@@ -0,0 +1,11 @@
+package modele.Comportement.Combat;
+
+public class ComportementAvecPistolet extends ComportementCombatAbstract {
+
+	@Override
+	public String Attaquer() {
+		
+		return "Je combat avec un Pistolet !";
+	}
+
+}
