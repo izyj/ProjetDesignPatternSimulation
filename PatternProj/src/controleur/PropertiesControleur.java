422f8037a9611ed7c87acc91cb2a26258a284fa2
Gaëtan <gae.marzilli@gmail.com>
2016-01-10 13:20:55 +0100

    Gestion du fichier properties + centralisation de la gestion des exeptions

--- PatternProj/src/controleur/PropertiesControleur.java
+++ PatternProj/src/controleur/PropertiesControleur.java
@@ -1,5 +1,32 @@
 package controleur;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertiesControleur {
 
+	   
+	/**
+	 * Récuperation des données du fichier properties.
+	 * @return properties
+	 */
+	public static Properties getProperties(String path){
+		
+		// Intervertir les lignes de commentaires pour test sur eclipse
+//		String configPath="src/properties/PatternOGame.properties";
+		String configPath="PatternOGame.properties";
+		
+		Properties properties = new Properties();
+		try {
+			FileInputStream in = new FileInputStream(configPath);
+			properties.load(in);
+			in.close();
+		} catch (IOException e) {
+			GestionExeptions.ecritureExeption("erreur_chargement_fichier_properties");
+		}
+		
+		return properties;
+	}
+	
 }
--- PatternProj/src/modele/Shape.java
+++ PatternProj/src/modele/Shape.java
@@ -2,9 +2,6 @@ package modele;
 
 public interface Shape {
 
-
-
-
 	public int getVie();
 	public void setVie(int vie);
 	public int getForce();
