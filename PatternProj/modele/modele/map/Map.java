1f1f2b17df01b2e361072967beb7a507f8a583ba
izyj <iisiramen@gmail.com>
2016-01-18 11:17:32 +0100

    MAJ

---
+++ PatternProj/modele/modele/map/Map.java
@@ -0,0 +1,31 @@
+package modele.map;
+
+import java.util.List;
+
+import modele.Personnage;
+import modele.ObjectItem.ObjectItemAbstract;
+
+import modele.Zone.Zone;
+
+public abstract class Map{
+
+	public List<Zone> Zones;
+
+    public abstract void AddZone(int x, int y, int z);
+    public abstract Zone GetZone(int x, int y, int z);
+
+
+
+    public abstract void ChargerObjet(List<ObjectItemAbstract> objets);
+    public abstract void ChargerPersonage(List<Personnage> personages);
+
+    public abstract void SupprimerObjet(ObjectItemAbstract obj);
+    public abstract void SupprimePersonage(Personnage perso);
+
+    public abstract void AjouterPersonageInZone(Personnage perso, Zone position);
+
+    public abstract void AfficherEnvironment(Personnage personage);
+
+
+
+}
