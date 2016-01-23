40e083442a7f48aa874a2fb02e7e903b87721d43
Gaëtan <gae.marzilli@gmail.com>
2016-01-23 15:58:23 +0100

    Images

---
+++ PatternProj/modele/modele/Iinterface/IGestionImages.java
@@ -0,0 +1,14 @@
+package modele.Iinterface;
+
+public interface IGestionImages {
+	
+	public String personnage = "ProjetDesignPatternSimulation\\PatternProj\\ressources\\images\\personnage.png";
+	public String mur = "ProjetDesignPatternSimulation\\PatternProj\\ressources\\images\\mur.png";
+	public String fin = "ProjetDesignPatternSimulation\\PatternProj\\ressources\\images\\fin.png";
+	public String food = "ProjetDesignPatternSimulation\\PatternProj\\ressources\\images\\food.png";
+	public String monstre = "ProjetDesignPatternSimulation\\PatternProj\\ressources\\images\\monstre.png";
+	public String arme = "ProjetDesignPatternSimulation\\PatternProj\\ressources\\images\\arme.png";
+	public String guardTower = "ProjetDesignPatternSimulation\\PatternProj\\ressources\\images\\guardTower.png";
+	
+}
+
